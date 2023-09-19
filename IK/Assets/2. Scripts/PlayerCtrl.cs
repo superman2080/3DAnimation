7c778d9cd6399274dbd210bf0a124441bfcc409f
superman2080 <superman2080@naver.com>
2023-09-19 17:27:26 +0900

    IK

--- IK/Assets/2. Scripts/PlayerCtrl.cs
+++ IK/Assets/2. Scripts/PlayerCtrl.cs
@@ -11,6 +11,14 @@ public class PlayerCtrl : MonoBehaviour
     [Range(0.5f, 2.5f)]
     public float mouseSensation;
     public float speed;
+    public Transform mouseTr;
+    public Transform lookAt;
+
+    [Range(2f, 5f)]
+    public float senseDist;
+
+    private Transform rightHand;
+
     private bool canMove = true;
     private Rigidbody rb;
     private Vector3 dir;
@@ -27,7 +35,6 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         PlayerMove();
         PlayerAnim();
     }
@@ -44,9 +51,9 @@ public class PlayerCtrl : MonoBehaviour
         rotX = Mathf.Repeat(rotX + Input.GetAxis("Mouse X") * mouseSensation, 360f);
         rotY = Mathf.Clamp(rotY + Input.GetAxis("Mouse Y") * mouseSensation, -90, 90);
 
-        Camera.main.transform.localEulerAngles = new Vector3(-rotY, 0, 0);
+        mouseTr.localEulerAngles = new Vector3(-rotY, 0, 0);
         transform.eulerAngles = (new Vector3(0, rotX, 0));
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
             StartCoroutine(DelayedJump(0.5f));
     }
 
@@ -61,7 +68,45 @@ public class PlayerCtrl : MonoBehaviour
     private void PlayerAnim()
     {
         animator.SetBool("Walk", dir != Vector3.zero);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && rb.velocity.y == 0)
             animator.SetTrigger("Jump");
+
+
+        Collider[] touchables = Physics.OverlapSphere(transform.position, senseDist, 1 << LayerMask.NameToLayer("Touchable"));
+        if (touchables.Length <= 0)
+        {
+            rightHand = null;
+        }
+        else
+        {
+            float dist = Vector3.Distance(transform.position, touchables[0].transform.position);
+            foreach (var item in touchables)
+            {
+                if (rightHand != null)
+                {
+                    if (Vector3.Distance(transform.position, item.transform.position) < dist)
+                        rightHand = item.transform;
+                }
+                else
+                    rightHand = item.transform;
+            }
+        }
+    }
+
+    void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            // Set the look target position, if one has been assigned
+            if (lookAt != null)
+            {
+                animator.SetLookAtWeight(1);
+                animator.SetLookAtPosition(lookAt.position);
+            }
+            if(rightHand != null) { 
+                animator.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
+                animator.SetIKPositionWeight(AvatarIKGoal.RightHand, (senseDist - Vector3.Distance(transform.position, rightHand.position)) / senseDist);
+            }
+        }
     }
 }
